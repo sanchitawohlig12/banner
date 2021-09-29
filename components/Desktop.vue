d682207f9e68d109fe49a220141c1deaae571581
sanchitagithub12 <sanchitagithub@gmail.com>
2021-09-29 12:29:15 +0530

    completed

---
+++ components/Desktop.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="!$vuetify.breakpoint.smAndDown">
+    <img class="one" src="/images/one.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.one {
+  width: 100%;
+  height: 100%;
+  /* position: absolute; */
+}
+</style>
---
+++ components/Mobile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-if="$vuetify.breakpoint.smAndDown">
+    <img class="two" src="/mobile/375-667.jpg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.two {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+</style>
